95e6859937abc1ac68833bf9f33805296797d619
Cal Colistra <calcolistra10@gmail.com>
2022-09-21 23:21:36 -0400

    remove grid

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ const ambientLight = new THREE.AmbientLight(0xffffff);  //ambient/flood light
 scene.add(pointLight, ambientLight)
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
+//const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
